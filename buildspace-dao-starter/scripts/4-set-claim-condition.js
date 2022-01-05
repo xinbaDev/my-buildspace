c8752ee5835ee6a28b396ea779b74947cd2c680a
xinbaDev <xinbagamer@gmail.com>
2022-01-05 18:05:19 +1100

    done dao exmaple

---
+++ buildspace-dao-starter/scripts/4-set-claim-condition.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+    "0x2e9f66e421c54374d9f6dB5D364bb1f8A1163aE4",
+);
+
+(async () => {
+    try {
+        const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+        // Specify conditions.
+        claimConditionFactory.newClaimPhase({
+            startTime: new Date(),
+            maxQuantity: 50_000,
+            maxQuantityPerTransaction: 1,
+        });
+
+
+        await bundleDrop.setClaimCondition(0, claimConditionFactory);
+        console.log("✅ Successfully set claim condition on bundle drop:", bundleDrop.address);
+    } catch (error) {
+        console.error("Failed to set claim condition", error);
+    }
+})()
