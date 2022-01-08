d13a850c243ea6e5ca6914a6b6b75945458eac3b
xinbaDev <xinbagamer@gmail.com>
2022-01-08 22:54:54 +1100

    add epic game

---
+++ epic-game/buildspace-nft-game-starter/src/constants.js
@@ -0,0 +1,14 @@
+const CONTRACT_ADDRESS = '0xce4364CECC079439A829eD2Df11Cca46e943adFF';
+
+
+const transformCharacterData = (characterData) => {
+    return {
+        name: characterData.name,
+        imageURI: characterData.imageURI,
+        hp: characterData.hp.toNumber(),
+        maxHp: characterData.maxHp.toNumber(),
+        attackDamage: characterData.attackDamage.toNumber(),
+    };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
